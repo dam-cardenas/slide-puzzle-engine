a06cec188dd37bdc5d2c9e08100c71e5c21aecc7
Damian Cardenas <damian.cardenas.developer@gmail.com>
2023-06-25 01:22:05 -0600

    nuevos cambios: -  generacion de tableros basados en moviemientos aleatorios - nueva interfaz cli para jugar  con el puzzle desde terminal!

--- src/index.ts
+++ src/index.ts
@@ -1,17 +1,75 @@
-import Board, { Direction } from "./lib/Board";
-const gameBoard = new Board({ width: 3, height: 3 })
-
-console.log({
-  position: gameBoard.position,
-  solved: gameBoard.solved,
+import { Board, Direction } from "./lib/Board";
+import { createInterface  } from 'readline';
+const gameBoard = new Board({ 
+  dimensions: { width: 3, height: 3 },
 })
 
-gameBoard.move([
-  Direction.up,
-  Direction.left,
-])
+const puzzleString = (board: Board) => {
+  const boardArray = board.to2dArray()
+  let prompt = ''
+  boardArray.forEach(row => {
+    prompt = prompt + row.join(', ') + '\n'
+  })
+  return prompt
+}
 
-console.log({
-  position: gameBoard.position,
-  solved: gameBoard.solved,
+const getPrompt = (movement = false) => {
+  return `Puzzler:
+${puzzleString(gameBoard)}
+${movement ? 'movement>\n' : ''}`
+}
+
+const readline = createInterface({
+  input: process.stdin,
+  output: process.stdout,
+  prompt: `Puzzler:
+${puzzleString(gameBoard)}
+movement>
+`
+});
+
+console.log(`
+controls:
+press:
+   ,---,
+   | W |
+,--',--',---,
+| A | S | D |
+'---'---'---'
+
+then enter
+`)
+readline.prompt()
+
+readline.on("line", line => {
+  switch (line.trim()) {
+    case 'up':
+    case 'w':
+      gameBoard.move([Direction.up])
+      break;
+    case 'down':
+    case 's':
+      gameBoard.move([Direction.down])
+      break;
+    case 'left':
+    case 'a':
+      gameBoard.move([Direction.left])
+      break;
+    case 'right':
+    case 'd':
+      gameBoard.move([Direction.right])
+      break;
+    default:
+      console.log(`options available: up [w] , down [s], left [a]  & right [d]`);
+      break;
+  }
+  if (gameBoard.solved) {
+    console.log(getPrompt())
+    console.log('puzzle solved, thanks for playing')
+    return readline.close()
+  }
+  readline.setPrompt(getPrompt(true))
+  readline.prompt(true)
+}).on('close', () => {
+  process.exit(0);
 })
--- src/lib/Board.ts
+++ src/lib/Board.ts
@@ -1,6 +1,9 @@
 
 interface boardDimension { width: number, height: number }
 interface position { x: number, y: number }
+type keyValue = [ string, number | null ]
+interface boardConfig { dimensions: boardDimension, board?:  keyValue[] }
+
 type puzzlePiece = number | null
 
 function getRandomInt(min: number, max: number) {
@@ -21,89 +24,134 @@ export enum BoardStatus {
   solved, 
 }
 
-export default class Board{
+export class Board{
   private board: Map<string, puzzlePiece>
   private solution: Map<string, puzzlePiece>
   private dimension: boardDimension
   private status: BoardStatus
 
-  // TODO: agregar posibilidad de generar un tablero con un preset
-  constructor(dimensions: boardDimension) {
-    const { width, height } = dimensions
+  constructor(config: boardConfig) {
+    const { dimensions } = config
     this.dimension = dimensions
-    this.board = new Map<string, puzzlePiece>()
-    this.solution = new Map<string, puzzlePiece>()
-    const pieces = this.generateBoardPieces()
-    const totalPieces = pieces.length
-    let solutionCount = 1
-    for (let heightCursor = 0; heightCursor < height; heightCursor++) {
-      for (let widthCursor = 0; widthCursor < width; widthCursor++) {
-        this.board.set(`${heightCursor}-${widthCursor}`, this.getRandomPiece(pieces))
-        this.solution.set(`${heightCursor}-${widthCursor}`, solutionCount !== totalPieces
-          ? solutionCount++
-          : null
-        )
-      }
-    }
+    const { solution, board } = this.generateBoard(config)
+    this.board = board
+    this.solution = solution
+    this.move(this.getRandomMovements(20))
     this.status = BoardStatus.ready
   }
 
   //#region Methods
-  private generateBoardPieces(): puzzlePiece[] {
-    const { width, height } = this.dimension
+  generateBoard(config: boardConfig) {
+    try {
+      const pieces = this.generateBoardPieces(config.dimensions)
+      const totalPieces = pieces.length
+      if (config.board && totalPieces !== config.board.length)
+        throw new Error(`Bad board configuration: wrong key length, length may be ${totalPieces}`)
+      const keys = []
+      for (let y = 0; y < config.dimensions.height; y++) for (let x = 0; x < config.dimensions.width; x++) keys.push(`${x}-${y}`)
+      const board = config.board 
+        ? new Map<string, puzzlePiece>(config.board)
+        : new Map<string, puzzlePiece>()
+      const boardKeys = [...board.keys()]
+      if (config.board && JSON.stringify(boardKeys) !== JSON.stringify(keys))
+        throw new Error(`Bad board configuration: keys does not match`)
+      const solution = new Map<string, puzzlePiece>()
+      let solutionCount = 1
+      const generator = config.board 
+        ? (key:string) => {
+          solution.set(
+            key, 
+            solutionCount !== totalPieces
+              ? solutionCount++
+              : null
+          )
+        }
+        : (key:string) => {
+          board.set(
+            key, 
+            solutionCount !== totalPieces
+              ? solutionCount
+              : null
+          )
+          solution.set(
+            key, 
+            solutionCount !== totalPieces
+              ? solutionCount++
+              : null
+          )
+        }
+      keys.forEach(key => generator(key))
+      return { board, solution }
+    } catch (error) {
+      console.log('Generate board error >>>>', error)
+      return {
+        board: new Map<string, puzzlePiece>(),
+        solution: new Map<string, puzzlePiece>(),
+      }
+    }
+  }
+
+  private generateBoardPieces(dimension:boardDimension): puzzlePiece[] {
+    const { width, height } = dimension
     const totalPieces = width * height - 1
     const pool: puzzlePiece[] = [null]
     for (let index = 1; index <= totalPieces; index++) pool.push(index)
     return pool
   }
 
-  private getRandomPiece(piecesPool: puzzlePiece[]): puzzlePiece {
-    const piecePosition = getRandomInt(0, piecesPool.length)
-    const piece = piecesPool[piecePosition]
-    piecesPool.splice(piecePosition, 1)
-    return piece
+  private getRandomMovements(movementsAmount: number): Direction[] {
+    const movements: Direction[] = []
+    for (let index = 0; index < movementsAmount; index++)
+      movements.push(getRandomInt(Direction.up, Direction.right + 1))
+    return movements
   }
 
   private moveUp() {
     if (!this.ableToMoveUp) return
-    const actualPosition = this.position 
-    const tempPiece = this.board.get(`${actualPosition.y - 1}-${actualPosition.x}`)
+    const { x, y } = this.position 
+    const key = `${x}-${y}`
+    const tempKey = `${x}-${y - 1}`
+    const tempPiece = this.board.get(tempKey)
     if (!tempPiece) return
-    this.board.set(`${actualPosition.y - 1}-${actualPosition.x}`, null)
-    this.board.set(`${actualPosition.y}-${actualPosition.x}`, tempPiece)
+    this.board.set(tempKey, null)
+    this.board.set(key, tempPiece)
   }
   
   private moveDown() {
     if (!this.ableToMoveDown) return
-    const actualPosition = this.position 
-    const tempPiece = this.board.get(`${actualPosition.y + 1}-${actualPosition.x}`)
+    const { x, y } = this.position 
+    const key = `${x}-${y}`
+    const tempKey = `${x}-${y + 1}`
+    const tempPiece = this.board.get(tempKey)
     if (!tempPiece) return
-    this.board.set(`${actualPosition.y + 1}-${actualPosition.x}`, null)
-    this.board.set(`${actualPosition.y}-${actualPosition.x}`, tempPiece)
+    this.board.set(tempKey, null)
+    this.board.set(key, tempPiece)
   }
 
   private moveLeft() {
     if (!this.ableToMoveLeft) return
-    const actualPosition = this.position 
-    const tempPiece = this.board.get(`${actualPosition.y}-${actualPosition.x - 1}`)
+    const { x, y } = this.position 
+    const key = `${x}-${y}`
+    const tempKey = `${x - 1}-${y}`
+    const tempPiece = this.board.get(tempKey)
     if (!tempPiece) return
-    this.board.set(`${actualPosition.y}-${actualPosition.x - 1}`, null)
-    this.board.set(`${actualPosition.y}-${actualPosition.x}`, tempPiece)
+    this.board.set(tempKey, null)
+    this.board.set(key, tempPiece)
   }
 
   private moveRight() {
     if (!this.ableToMoveRight) return
-    const actualPosition = this.position 
-    const tempPiece = this.board.get(`${actualPosition.y}-${actualPosition.x + 1}`)
+    const { x, y } = this.position 
+    const key = `${x}-${y}`
+    const tempKey = `${x + 1}-${y}`
+    const tempPiece = this.board.get(tempKey)
     if (!tempPiece) return
-    this.board.set(`${actualPosition.y }-${actualPosition.x + 1}`, null)
-    this.board.set(`${actualPosition.y}-${actualPosition.x}`, tempPiece)
+    this.board.set(tempKey, null)
+    this.board.set(key, tempPiece)
   }
 
   move( directions:Direction[]) {
-    console.log('start moving...')
     directions.forEach( direction => {
-      console.log('moving >>> ', direction)
       switch (direction) {
         case Direction.up:
           this.moveUp()
@@ -122,16 +170,34 @@ export default class Board{
       this.status = BoardStatus.solved
     }
   }
+
+  to2dArray() {
+    const { width,  height } = this.dimension
+    const board = []
+    for (let y = 0; y < height; y++) {
+      const column = []
+      for (let x = 0; x < width; x++) {
+        const key = `${x}-${y}`
+        column.push( this.board.get(key) ?? 'x' )        
+      }
+      board.push(column)
+    }
+    return board
+  }
+
+  toString() {
+    return JSON.stringify(this.to2dArray())
+  }
   //#endregion
   
-  //#region  properties
+  //#region properties
   get solved(): boolean {
     return JSON.stringify([...this.board.entries()]) === JSON.stringify([...this.solution.entries()]) 
   }
   get position(): position {
-    for (let y = 0; y < this.dimension.height; y++)
-      for (let x = 0; x < this.dimension.width; x++) {
-        const value = this.board.get(`${y}-${x}`)
+    for (let x = 0; x < this.dimension.width; x++)
+      for (let y = 0; y < this.dimension.height; y++) {
+        const value = this.board.get(`${x}-${y}`)
         if (!value) return { x, y }
       }
     return { x: -1, y: -1 }
